bbb54e63ae5d8aa0f9c0587425a5baa8661ea605
= <c0m47053@gmail.com>
2012-09-25 11:50:56 +0100

    Problem 3 solved, but slow

---
+++ problem3.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+
+def is_prime(num):
+	x = 2
+	while x < num - 1:
+		if num % x == 0:
+			return False
+		x = x + 1
+	return True
+
+
+def largest_prime_factor(num):
+	divisor = num
+	while divisor > 2:
+		print divisor, " - ", num % divisor
+		if num % divisor == 0 and is_prime(divisor):
+			return divisor 
+		divisor = divisor - 1
+	return 0
+
+print largest_prime_factor(600851475143)
+
+
